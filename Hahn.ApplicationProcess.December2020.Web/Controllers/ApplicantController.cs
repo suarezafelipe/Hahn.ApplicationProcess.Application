fdff6812040cd5c0593b5bb6d7b82d48beaf09d9
Felipe Suarez <suarez.a.felipe@gmail.com>
2021-01-13 14:02:52 -0500

    Add Applicant entity and controller. Add Data and Domain projects

---
+++ Hahn.ApplicationProcess.December2020.Web/Controllers/ApplicantController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hahn.ApplicationProcess.December2020.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApplicantController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok();
+        }
+
+    }
+}
