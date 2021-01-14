503c528cfe71ea7476adacffa4706cc56df024b2
Felipe Suarez <suarez.a.felipe@gmail.com>
2021-01-14 14:08:33 -0500

    Add comment for deprecated use of validation mode

--- Hahn.ApplicationProcess.December2020.Domain/Validators/ApplicantValidator.cs
+++ Hahn.ApplicationProcess.December2020.Domain/Validators/ApplicantValidator.cs
@@ -14,11 +14,14 @@ namespace Hahn.ApplicationProcess.December2020.Domain.Validators
             RuleFor(x => x.Name).MinimumLength(5);
             RuleFor(x => x.FamilyName).MinimumLength(5);
             RuleFor(x => x.Address).MinimumLength(10);
-            RuleFor(r => r.CountryOfOrigin).Must(ValidateCountry)
-                .WithMessage("CountryOfOrigin not found. Try with the english name or with the 2 letter ISO code");
-            RuleFor(x => x.EmailAddress).EmailAddress(EmailValidationMode.Net4xRegex);
             RuleFor(x => x.Age).InclusiveBetween(20, 60);
             RuleFor(x => x.Hired).NotNull();
+
+            // This validation was asked in the requirements, so we are using Net4xRegex validation mode even though is deprecated
+            RuleFor(x => x.EmailAddress).EmailAddress(EmailValidationMode.Net4xRegex);
+
+            RuleFor(r => r.CountryOfOrigin).Must(ValidateCountry)
+                .WithMessage("CountryOfOrigin not found. Try with the english name or with the 2 letter ISO code");
         }
 
         private static bool ValidateCountry(Applicant applicant, string country)
