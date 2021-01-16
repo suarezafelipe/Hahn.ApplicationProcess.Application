f7d4180fd642ca491952235eefaa6de335110820
Felipe Suarez <suarez.a.felipe@gmail.com>
2021-01-16 18:19:32 -0500

    Add aurelia-validation and add logic to validate the form

--- Hahn.ApplicationProcess.December2020.Web/front-app/src/applicant/applicant-form.ts
+++ Hahn.ApplicationProcess.December2020.Web/front-app/src/applicant/applicant-form.ts
@@ -1,3 +1,10 @@
+import { bindable, inject } from "aurelia-framework";
+import {
+  ValidationRules,
+  ValidationControllerFactory,
+  ValidationController,
+} from "aurelia-validation";
+
 interface Applicant {
   name: string;
   familyName: string;
@@ -8,14 +15,51 @@ interface Applicant {
   hired: boolean;
 }
 
+@inject(ValidationControllerFactory)
 export class ApplicantForm {
   public message = "hello from applicant form";
-  applicant: Applicant;  
+  @bindable applicant: Applicant;
+  pageJustLoaded: boolean;
+  controller: ValidationController;
+  isEmptyApplicant: boolean;
+
+  constructor(ValidationControllerFactory) {
+    this.controller = ValidationControllerFactory.createForCurrentScope();
+    this.pageJustLoaded = true;
+    this.isEmptyApplicant = true;
+  }
 
   submit() {
-    console.log(this.applicant);    
+    this.controller.validate();
+
+    console.log(this.applicant);
     // To prevent default browser's behaviour of reloading the page
     return false;
   }
-}
 
+  applicantChanged() {
+    this.controller.validate();
+    this.pageJustLoaded = false;
+    if (this.applicant) {
+      ValidationRules.ensure((a: Applicant) => a.name)
+        .required()
+        .minLength(5)
+        .ensure((a: Applicant) => a.familyName)
+        .required()
+        .minLength(5)
+        .ensure((a: Applicant) => a.address)
+        .required()
+        .minLength(10)
+        .ensure((a: Applicant) => a.email)
+        .required()
+        .email()
+        .ensure((a: Applicant) => a.age)
+        .required()
+        .range(20, 60)
+        .on(this.applicant);
+      this.isEmptyApplicant = false;
+    } else {
+      this.isEmptyApplicant = true;
+    }
+  }
+}
