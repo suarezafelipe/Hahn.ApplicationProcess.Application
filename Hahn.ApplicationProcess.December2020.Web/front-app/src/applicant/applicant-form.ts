dc7ccb9598803c5bd8063c503f71ed30c556547f
Felipe Suarez <suarez.a.felipe@gmail.com>
2021-01-16 21:19:04 -0500

    Submit and Reset buttons working according to specification

--- Hahn.ApplicationProcess.December2020.Web/front-app/src/applicant/applicant-form.ts
+++ Hahn.ApplicationProcess.December2020.Web/front-app/src/applicant/applicant-form.ts
@@ -1,65 +1,124 @@
-import { bindable, inject } from "aurelia-framework";
+import { observable, inject } from "aurelia-framework";
 import {
   ValidationRules,
   ValidationControllerFactory,
   ValidationController,
 } from "aurelia-validation";
 
-interface Applicant {
-  name: string;
-  familyName: string;
-  address: string;
-  countryOfOrigin: string;
-  email: string;
-  age: number;
-  hired: boolean;
-}
-
 @inject(ValidationControllerFactory)
 export class ApplicantForm {
   public message = "hello from applicant form";
-  @bindable applicant: Applicant;
-  pageJustLoaded: boolean;
+
+  @observable name: string;
+  @observable familyName: string;
+  @observable address: string;
+  @observable countryOfOrigin: string;
+  @observable email: string;
+  @observable age: number;
+  @observable hired: boolean;
+
   controller: ValidationController;
   isEmptyApplicant: boolean;
+  isApplicantComplete: boolean;
 
   constructor(ValidationControllerFactory) {
     this.controller = ValidationControllerFactory.createForCurrentScope();
-    this.pageJustLoaded = true;
+    this.isApplicantComplete = false;
     this.isEmptyApplicant = true;
   }
 
-  submit() {
-    this.controller.validate();
-
-    console.log(this.applicant);
-    // To prevent default browser's behaviour of reloading the page
-    return false;
+  created() {
+    ValidationRules.ensure("name")
+      .required()
+      .minLength(5)
+      .ensure("familyName")
+      .required()
+      .minLength(5)
+      .ensure("address")
+      .required()
+      .minLength(10)
+      .ensure("email")
+      .required()
+      .email()
+      .ensure("age")
+      .required()
+      .range(20, 60)
+      .on(ApplicantForm);
   }
 
-  applicantChanged() {
-    this.controller.validate();
-    this.pageJustLoaded = false;
-    if (this.applicant) {
-      ValidationRules.ensure((a: Applicant) => a.name)
-        .required()
-        .minLength(5)
-        .ensure((a: Applicant) => a.familyName)
-        .required()
-        .minLength(5)
-        .ensure((a: Applicant) => a.address)
-        .required()
-        .minLength(10)
-        .ensure((a: Applicant) => a.email)
-        .required()
-        .email()
-        .ensure((a: Applicant) => a.age)
-        .required()
-        .range(20, 60)
-        .on(this.applicant);
-      this.isEmptyApplicant = false;
-    } else {
-      this.isEmptyApplicant = true;
+  submit() {
+    console.log("submit triggered!");
+    console.log(this.name);
+    console.log(this.age);
+    console.log(this.hired);
+  }
+
+  nameChanged() {
+    this.isEmptyApplicant = this.isFormEmpty();
+    this.isApplicantComplete = this.isFormFilled();
+  }
+
+  familyNameChanged() {
+    this.isEmptyApplicant = this.isFormEmpty();
+    this.isApplicantComplete = this.isFormFilled();
+  }
+
+  emailChanged() {
+    this.isEmptyApplicant = this.isFormEmpty();
+    this.isApplicantComplete = this.isFormFilled();
+  }
+
+  addressChanged() {
+    this.isEmptyApplicant = this.isFormEmpty();
+    this.isApplicantComplete = this.isFormFilled();
+  }
+
+  ageChanged() {
+    this.isEmptyApplicant = this.isFormEmpty();
+    this.isApplicantComplete = this.isFormFilled();
+  }
+
+  isFormEmpty() {
+    let isEmpty = true;
+
+    if (
+      this.name ||
+      this.familyName ||
+      this.address ||
+      this.countryOfOrigin ||
+      this.email ||
+      this.age
+    ) {
+      isEmpty = false;
     }
+
+    return isEmpty;
+  }
+
+  isFormFilled() {
+    let isFilled = false;
+
+    if (
+      this.name &&
+      this.familyName &&
+      this.address &&
+      this.countryOfOrigin &&
+      this.email &&
+      this.age
+    ) {
+      isFilled = true;
+    }
+
+    return isFilled;
+  }
+
+  reset() {
+    this.name = "";
+    this.familyName = "";
+    this.countryOfOrigin = "";
+    this.address = "";
+    this.email = "";
+    this.age = null;
+    this.hired = false;
   }
 }
